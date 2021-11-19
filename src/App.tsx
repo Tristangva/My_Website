8f6d35e05bbee930f36db0cb24849b17265ca84d
Tristan White <tristanwhite.gva@gmail.com>
2021-11-18 18:02:33 -0600

    Navbar uses Bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import logo from './logo.svg';
 //import './App.css';
 import Nav_Bar from "./components/Nav_Bar";
-import Socials from "./components/Socials";
 import Footer from "./components/Footer";
 
 function App() {
--- src/components/Nav_Bar.tsx
+++ src/components/Nav_Bar.tsx
@@ -1,76 +1,54 @@
 import React from "react";
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    NavLink
-} from 'react-router-dom';
+import { Nav, Navbar, Container } from "react-bootstrap";
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import About from "./About";
 import Contact from "./Contact";
 import Main from "./main";
 import Projects from "./Projects";
 import Resume from "./Resume";
 
-// Version 2 will have better code
-function Nav_Bar(props: any) {
+const Bootstrap_Nav = () => {
+    return(
+        <Navbar collapseOnSelect fixed={'top'} expand={'sm'} bg={'dark'} variant={'dark'}>
+            <Container>
+                <Navbar.Toggle aria-controls={ 'responsive-nav-bar'} />
+                <Navbar.Collapse id={ 'responsive-nav-bar'}>
+                    <Nav>
+                        <Nav.Link href={'/'}>Home</Nav.Link>
+                        <Nav.Link href={'/about'}> About </Nav.Link>
+                        <Nav.Link href={'projects'}> Projects </Nav.Link>
+                        <Nav.Link href={'/resume'}> Resume </Nav.Link>
+                        <Nav.Link href={'/contact'}> Contact</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+const Nav_Bar = () => {
+
     return (
         <div className={"nav-bar"}>
             <Router>
-                <nav>
-                    <li>
-                        <NavLink
-                            to="/"
-                        >
-                            Home
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink
-                            to="/about"
-                        >
-                            About
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink
-                            to="/projects"
-                        >
-                            Projects
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink
-                            to="/resume"
-                        >
-                            Resume
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink
-                            to="/contact"
-                        >
-                            Contact
-                        </NavLink>
-                    </li>
-                    <Routes>
-                        <Route path="/"
-                               element={<Main/>}>
-                        </Route>
-                        <Route path= "/about"
-                               element={ <About />}>
-                        </Route>
-                        <Route path="/projects"
-                               element={ <Projects />}>
-                        </Route>
-                        <Route path="/resume"
-                               element={ <Resume />}>
-                        </Route>
-                        <Route path="/contact"
-                               element={ <Contact />}>
-                        </Route>
-
-                    </Routes>
-                </nav>
+                <Bootstrap_Nav />
+                <Routes>
+                    <Route path= "/about"
+                           element={ <About />}>
+                    </Route>
+                    <Route path="/projects"
+                           element={ <Projects />}>
+                    </Route>
+                    <Route path="/resume"
+                           element={ <Resume />}>
+                    </Route>
+                    <Route path="/contact"
+                           element={ <Contact />}>
+                    </Route>
+                    <Route path="/"
+                           element={<Main/>}>
+                    </Route>
+                </Routes>
             </Router>
         </div>
     );
