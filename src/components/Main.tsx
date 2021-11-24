e9305303ab77379805fa2624a20233fe2fcbd50e
Tristan White <tristanwhite.gva@gmail.com>
2021-11-24 15:14:39 -0600

    Profile pic added, some now css

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import profile from '../assets/Profile_Pic.svg';
 import "./Main.css";
 
 const Main = () => {
     return (
-        <div className="main">
-            <h2>Hello I am Tristan White</h2>
-            <p>Newly graduated Computer Science major</p>
-        </div>
+        <Container className="main">
+            <Row>
+                <Col className={'intro'}>
+                    <h2>Hello, I am Tristan White</h2>
+                    <p>I am a newly graduated Computer Science major wishing to enter into the workforce as a Software Developer or Software Engineer</p>
+                </Col>
+                <Col className={'profile-pic'}>
+                    <img src={profile} className="img-fluid" />
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
