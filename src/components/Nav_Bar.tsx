5d7bbecc04871fdae0b4a17d0dcf9a807849105d
Tristan White <tristanwhite.gva@gmail.com>
2021-11-25 17:00:30 -0600

    First deployed version

--- src/components/Nav_Bar.tsx
+++ src/components/Nav_Bar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Nav, Navbar, Container } from "react-bootstrap";
-import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import About from "./About";
 import Contact from "./Contact";
 import Main from "./Main";
@@ -14,13 +14,13 @@ const Bootstrap_Nav = () => {
             <Container>
                 <Navbar.Toggle aria-controls={ 'responsive-nav-bar'} />
                 <Navbar.Collapse id={ 'responsive-nav-bar'}>
-                    <Nav>
-                        <Nav.Link href={'/'}>Home</Nav.Link>
-                        <Nav.Link href={'/about'}> About </Nav.Link>
-                        <Nav.Link href={'projects'}> Projects </Nav.Link>
-                        <Nav.Link href={'/resume'}> Resume </Nav.Link>
-                        <Nav.Link href={'/contact'}> Contact</Nav.Link>
-                    </Nav>
+                        <Nav>
+                            <Nav.Link href={'#/'}>Home</Nav.Link>
+                            <Nav.Link href={'#/about'}> About </Nav.Link>
+                            <Nav.Link href={'#/projects'}> Projects </Nav.Link>
+                            <Nav.Link href={'#/resume'}> Resume </Nav.Link>
+                            <Nav.Link href={'#/contact'}> Contact</Nav.Link>
+                        </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
@@ -28,29 +28,16 @@ const Bootstrap_Nav = () => {
 }
 
 const Nav_Bar = () => {
-
     return (
         <div className={"nav-bar"}>
-            <Router>
-                <Bootstrap_Nav />
-                <Routes>
-                    <Route path= "/about"
-                           element={ <About />}>
-                    </Route>
-                    <Route path="/projects"
-                           element={ <Projects />}>
-                    </Route>
-                    <Route path="/resume"
-                           element={ <Resume />}>
-                    </Route>
-                    <Route path="/contact"
-                           element={ <Contact />}>
-                    </Route>
-                    <Route path="/"
-                           element={<Main/>}>
-                    </Route>
-                </Routes>
-            </Router>
+            <Bootstrap_Nav />
+            <Routes>
+                <Route path= "/about" element={ <About />} />
+                <Route path="/projects" element={ <Projects />} />
+                <Route path="/resume" element={ <Resume />} />
+                <Route path="/contact" element={ <Contact />} />
+                <Route path="/" element={<Main/>} />
+            </Routes>
         </div>
     );
 }
