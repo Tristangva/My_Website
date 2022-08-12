a19608442d278bb313733aa1e871ef426179cb0b
Tristan White <tristanwhite.gva@gmail.com>
2022-08-12 15:58:41 -0500

    Updates

---
+++ precache-manifest.e731bc31d0bdf8db3eaa88d15c07b619.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bacb5c4cf1547643bc26",
+    "url": "/static/css/main.33e33d0e.chunk.css"
+  },
+  {
+    "revision": "bacb5c4cf1547643bc26",
+    "url": "/static/js/main.f6293d52.chunk.js"
+  },
+  {
+    "revision": "ac0c87b051fe139efbd5",
+    "url": "/static/js/runtime~main.648dc5cb.js"
+  },
+  {
+    "revision": "6dde1d6619ce1c0a5cd4",
+    "url": "/static/js/2.155f4b44.chunk.js"
+  },
+  {
+    "revision": "37588c9075c6fc6ddfeb",
+    "url": "/static/js/3.544e8800.chunk.js"
+  },
+  {
+    "revision": "b0c424331ae9abae8855090097a9a584",
+    "url": "/static/media/Profile_Pic.b0c42433.svg"
+  },
+  {
+    "revision": "94b4f5b4b45e6abdbcb72fea9e6bdfc8",
+    "url": "/index.html"
+  }
+];
