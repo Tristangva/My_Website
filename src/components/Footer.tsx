00d059fe419b790d1e162211ef580a53e8908eae
Tristan White <tristanwhite.gva@gmail.com>
2022-08-13 11:31:05 -0500

    Fixed footer alignment

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,14 +10,17 @@ const Footer = () => {
     return (
         <Container fluid className={"footer"}>
             <Row>
-                <Col sm={6} className={"socials"}>
+                <Col className={"copyright"}>
+                    <Row className={"copyright-date"}>Copyright © {year} Tristan White</Row>
+
+                </Col>
+                <Col  className={"socials"}>
                     <Socials />
                 </Col>
-                <Col  className={"copyright"}>
-                    <p>Copyright © {year} Tristan White</p>
-                    <p className={"svg-copyright"}>The background, circuit-primary is a derivative of
+                <Col >
+                    <Row className={"svg-copyright"}>The background, circuit-primary is a derivative of
                         https://bgjar.com/circuit-primary by bgjar.com under CC by 4.0.
-                        This is licenced under CC by 40.0 by Tristan White </p>
+                        This is licenced under CC by 40.0 by Tristan White </Row>
                 </Col>
             </Row>
         </Container>
